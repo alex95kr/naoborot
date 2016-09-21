769945eea65977bb9b721796489e8c064880b9df
alex95kr <you@yourdomain.com>
2016-09-21 12:47:43 +0300

    рабочая версия 1.0

--- Naoborot/ViewController.swift
+++ Naoborot/ViewController.swift
@@ -9,6 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textchange: UITextField!
+    
+    
+    @IBAction func buttonchange(_ sender: UIButton) {
+        
+        var original : String = textchange.text!  // получаем из поля текст где мы его ввели
+        var mass = [Character] () // создаем массив для разложения исходного значения по буквам
+        
+        // в этом цикле из переменной original (string) разлаживается по символам в массив
+        for i in original.characters {
+            mass += ([i])
+        }
+        
+        // эта функция перелаживает в массиве данные задом наперед
+        mass.reverse ()
+        
+        // присваивание результата сразу полю где вводился текст, для присвоения ему значения так как он (string мы временно сделали из массива тип (string)
+        self.textchange.text=String(mass)
+        
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
